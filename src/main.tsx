5d021806bf62b0959bf5ad305ff2d2701e36808f
PerfZero <gundenis1@gmail.com>
2026-02-20 10:48:53 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "@fontsource-variable/geist";
+import "@fontsource-variable/geist/index.css";
 import "./index.css";
 import App from "./App.tsx";
 import { applyTheme, resolveInitialTheme } from "./shared/theme/theme";
